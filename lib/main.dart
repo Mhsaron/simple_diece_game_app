0201a0d49fee0f97e8f188e6758beab902e0e41e
Mhsaron <17-22808-1@student.aiub.edu>
2020-10-27 08:12:58 +0600

    flex

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Colors.white10,
       appBar: AppBar(
         title: Center(child: Text('Ludo_Dicee')),
         backgroundColor: Colors.blue,
@@ -19,6 +19,13 @@ class DicePage extends StatelessWidget {
     return Row(
       children: <Widget>[
         Expanded(
+          flex: 3,
+          child: Image(
+            image: AssetImage('images/1.png'),
+          ),
+        ),
+        Expanded(
+          flex: 1,
           child: Image(
             image: AssetImage('images/1.png'),
           ),
